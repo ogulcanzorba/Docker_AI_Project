94c02dedf4e9ce3b381de169530f6264f8364190
Olci <ogulcan1134@gmail.com>
2025-04-13 13:44:58 +0300

    Authentication System
    From now on we can create users, all users have their own chat history.

--- ai_project/settings.py
+++ ai_project/settings.py
@@ -9,27 +9,28 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5hu%(7*#%j66vd41m%#_1wi=orzk*xtfrv=0ny(w%vv26zog4h'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-5hu%(7*#%j66vd41m%#_1wi=orzk*xtfrv=0ny(w%vv26zog4h')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
+# For CSRF in Docker or cross-origin
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'http://host.docker.internal:8000',
+]
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -55,8 +56,7 @@ ROOT_URLCONF = 'ai_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,10 +71,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ai_project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -82,10 +80,8 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -101,25 +97,32 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Login settings
+LOGIN_URL = '/'
+LOGIN_REDIRECT_URL = '/chat/'
+LOGOUT_REDIRECT_URL = '/'
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_URL = '/static/'  # URL for static files
 
-STATIC_URL = 'static/'
+# For development, this will allow serving of static files from the 'static' folder
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Assumes static is in the same directory as manage.py
+]
+
+# Directory where collected static files will be stored (for production)
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Make sure this folder exists or is created during collectstatic
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
