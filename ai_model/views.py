5ed211ea6fe414edd87c37371728c7f95cfa8887
Olci <ogulcan1134@gmail.com>
2025-04-29 15:19:05 +0300

    Login/Signup Frontend Changes

--- ai_model/views.py
+++ ai_model/views.py
@@ -322,7 +322,10 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            messages.success(request, "Signup successful! You are now logged in.")
             return redirect('lecture_list')
+        else:
+            messages.error(request, "Signup failed. Please correct the errors below.")
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
