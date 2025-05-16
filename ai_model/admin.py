4c1b72be0a57658561d2dab448ffb75bff147362
Olci <ogulcan1134@gmail.com>
2025-05-16 20:18:52 +0300

    Update admin.py

--- ai_model/admin.py
+++ ai_model/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Quiz, ChatHistory  # Import your models
+from .models import Quiz, ChatHistory,Transcript # Import your models
 
 # Register the Quiz model
 class QuizAdmin(admin.ModelAdmin):
@@ -16,3 +16,13 @@ class ChatHistoryAdmin(admin.ModelAdmin):
     list_filter = ('lecture',)  # Add a filter for the lecture field
 
 admin.site.register(ChatHistory, ChatHistoryAdmin)  # Register the ChatHistory model with the custom admin class
+
+
+class TranscriptAdmin(admin.ModelAdmin):
+    list_display = ('user', 'lecture', 'created_at', 'pdf_file')  # Görünen sütunlar
+    search_fields = ('transcript_text', 'lecture', 'user__username')  # Arama yapılabilir alanlar
+    list_filter = ('lecture', 'created_at')  # Sağ tarafta filtreleme opsiyonları
+    readonly_fields = ('transcript_text', 'created_at')  # Sadece okumalık alanlar (değiştirilemez)
+
+
+admin.site.register(Transcript, TranscriptAdmin)  
