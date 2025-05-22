cdffbb751f2ab7937cf98aa1510a67660fd99012
pelineceburgun <pelinece03@gmail.com>
2025-05-22 08:13:32 +0300

    CI tests

--- ai_model/tests.py
+++ ai_model/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
+
+class BasicTests(TestCase):
+    def test_homepage(self):
+        response = self.client.get(reverse(''))  # urls.py’deki ana sayfa ismi neyse onu yaz
+        self.assertEqual(response.status_code, 200)
+
+    def test_sample_logic(self):
+        self.assertEqual(2 * 2, 4)
 
 # Create your tests here.
