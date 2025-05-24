6d169681c8106d7d522b2db45f9f49fcccfec871
Olci <ogulcan1134@gmail.com>
2025-05-24 18:58:00 +0300

    Postgre Image added

--- ai_project/settings.py
+++ ai_project/settings.py
@@ -71,15 +71,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ai_project.wsgi.application'
 
+from decouple import config
+import dj_database_url
+
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default='postgresql://myuser:mypassword@postgres:5432/mydb'),
+        conn_max_age=600
+    )
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
